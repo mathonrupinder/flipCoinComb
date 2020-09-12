7059362905b539f3e56ed3e663265bac58f9fe1a
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-12 10:11:17 +0530

    UC2:Store Singlet Combination and find percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,37 @@
 echo "Welcome to FLIP COIN Combination problem Solution"
-Head=0
-Tail=0
-flip=$(( RANDOM%2 ))
-if [ $flip -eq "1" ]
+function singlet(){
+declare -A  dict
+for (( i=1; i<=n; i++ ))
+do
+     flip=$(( RANDOM%2 ))
+      if [ $flip -eq 1 ]
+      then
+            ((dict[H]++))
+      else
+            ((dict[T]++))
+      fi
+done
+echo "Head="${dict[H]}
+echo "Tail="${dict[T]}
+     headPer=$(( ${dict[H]}*100/n ))
+     tailPer=$(( ${dict[T]}*100/n ))
+     if [ $headPer -gt $tailPer ]
 then
-    echo "head wins"
-else
-    echo "tail wins"
+     echo "Head percentage $headPer is maximum"
+     elif [ $tailPer  -gt $headPer ]
+then
+     echo "Tail percentage $tailPer is maximum"
+     else
+     echo "Both are equal"
 fi
+}
+read -p "Number of times loop to execute:" n
+echo "Enter 1 for singlet"
+read -p "Enter number for operation::" num
+    case $num in
+            1) singlet  ;;
+           
+*)
+                exit ;;
+    esac
+
